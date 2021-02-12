4d922b4d6b574113b9c97d99f0d5d3ee30ebd79d
enin11 <dominic_24comet@yahoo.com>
2021-02-12 00:52:40 -0800

    init

---
+++ project2/src/ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,44 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "ball_chaser/DriveToTarget.h"
+
+// ROS::Publisher motor commands;
+ros::Publisher motor_command_publisher;
+
+bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res)
+{
+    ROS_INFO("DriveToTargetRequest received - lx:%1.2f, az:%1.2f", (float)req.linear_x, (float)req.angular_z);
+
+    // Create a motor_command object of type geometry_msgs::Twist
+    geometry_msgs::Twist motor_command;
+    // Set wheel velocities, forward [0.5, 0.0]
+    motor_command.linear.x = req.linear_x;
+    motor_command.angular.z = req.angular_z;
+    // Publish angles to drive the robot
+    motor_command_publisher.publish(motor_command);
+
+    // Return message
+    //res.msg_feddback = "Motor Command Set - Linear_X: " + std::to_string(motor_command.linear.x) + " , Angular_Z: " + std::to_string(motor_command.angular.z);
+   
+    return true;
+}
+
+
+int main(int argc, char** argv)
+{
+    // Initialize a ROS node
+    ros::init(argc, argv, "drive_bot");
+
+    // Create a ROS NodeHandle object
+    ros::NodeHandle n;
+
+    // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
+    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    ros::ServiceServer service = n.advertiseService("/drive_bot/command_robot", handle_drive_request); 
+    ROS_INFO("Ready to move robot"); 
+
+    ros::spin();
+
+    return 0;
+}
